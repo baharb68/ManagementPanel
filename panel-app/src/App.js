37e4ac074d6d48c6e7e27c392a81c4e408547c45
bahar <bagheri_bhr@yahoo.com>
2019-09-24 17:10:16 +0330

    adding a dropdown button and adding name and profile props

--- panel-app/src/App.js
+++ panel-app/src/App.js
@@ -5,13 +5,14 @@ import 'jquery';
 import 'popper.js';
 import 'bootstrap';
 import 'vazir-font/dist/font-face.css';
+import user from '../src/components/img/user.png'
 import Navbar from '../src/components/Navbar/Navbar';
 import Main from '../src/components/Main/Main'
 
 function App() {
   return (
     <div className="App">
-        <Navbar logo="پنل مدیریت" />
+        <Navbar logo="پنل مدیریت" profileName="بهاره باقری" profileIcon={user} />
         <Main />
     </div>
   );
--- panel-app/src/components/Navbar/Navbar.jsx
+++ panel-app/src/components/Navbar/Navbar.jsx
@@ -1,9 +1,9 @@
 import React, { Component } from 'react'
 import './Navbar.css'
 import bell from '../img/bell.png'
-import user from '../img/user.png'
+
 import mail from '../img/mail-black-envelope-symbol.png'
-import logout from '../img/logout.png'
+import menu from '../img/menu.png'
 import Icon from './Icon'
 export default class Navbar extends Component {
     constructor(props){
@@ -12,21 +12,26 @@ export default class Navbar extends Component {
     render() {
         return (
             <div>
-                 <nav className="navbar navbar-expand-lg d-flex flex-wrap navbar-light bg-white p-0 b-shadow">
-                     <div className="part1 col-12 col-md-2 w-100 align-items-center">
-                        <a className="navbar-brand font-weight-bold" href="#">{this.props.logo}</a>
+                 <nav className="navbar navbar-expand-lg d-flex flex-wrap navbar-light bg-white p-0 b-shadow py-2">
+                     <div className="part1 col-12 col-md-2 w-100 align-items-center text-right">
+                        <a className="navbar-brand font-weight-bold m-0" href="#">{this.props.logo}</a>
                     </div>
                     <div className="part2 col d-flex justify-content-end pt-4 pt-md-0">
                         <Icon navicon={mail} number="3" />
                         <Icon navicon={bell} number="3" />
                     </div>
-                        <div className="pr-4">
-                            <img src={logout} alt=""/>
+                    <div class="btn-group dropright ml-4">
+                        <button type="button" class="btn dropdown-toggle" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                           <img src={menu} alt=""/>
+                        </button>
+                        <div class="dropdown-menu dropdown-menu-right">
+                            <button class="dropdown-item d-flex align-items-center py-2 text-right" type="button">
+                                <img src={this.props.profileIcon} alt=""/>
+                                <h6 className="pr-2">{this.props.profileName}</h6>
+                            </button>
+                            <button class="dropdown-item text-right py-2 font-weight-bold" type="button">خروج </button>
                         </div>
-                        <div className="w-25 ">
-                            <img src={user} alt=""/>
-                        </div>
-                    
+                    </div>
                 </nav>
             </div>
         )
